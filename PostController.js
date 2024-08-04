dbb14b775fa87990573907d9697e1dba55760141
hryntar <hrynchuktt@gmail.com>
2024-08-04 14:17:44 +0300

    delete Post controller

--- PostController.js
+++ PostController.js
@@ -45,6 +45,12 @@ class PostController {
    }
    async delete(req, res) {
       try {
+         const { id } = req.params;
+         if (!id) {
+            res.status(400).json({ message: "id is not specified" });
+         }
+         const post = await Post.findByIdAndDelete(id);
+         return res.json(post);
       } catch (error) {
          res.status(500).json(error);
       }
